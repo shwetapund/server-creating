82d1c7b23f691b1fd2cf3822bb344e9e11f22a79
shwetapund <pundshweta2020@gmail.com>
2023-10-18 19:54:20 -0700

    read the query parameter

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-import express from 'express'
+import express, { query } from 'express'
 
 const app = express();
 
-app.get('/jalebi',(req, res)=>{
-    res.send('Jalebi is sweet')
-})
-app.get('/samosa',(req,res)=>{
-    res.send('samosa is spicy')
-})
-app.get('/pizza',(req,res)=>{
-    res.send('pizza is so yummy')
-})
+app.get('/study',(req,res)=>{
+
+    const subject = req.query.subject;
+    const time = req.query.time;
+
+    console.log(req.query)
+    res.json({
+        message: `Studying ${subject}, for ${time} hours`
+    })
+});
+
 
 const PORT = 5000;
 
